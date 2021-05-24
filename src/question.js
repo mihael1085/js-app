d8d9081997dac9cfaaa5d8c3397711b8fc0017ba
mihael10 <mihael1085@gmail.com>
2021-05-24 19:53:52 +0300

    add function : save and load data from LocalStorage

--- src/question.js
+++ src/question.js
@@ -10,7 +10,46 @@ export class Question {
 			.then(response => response.json())
 			.then(response => {
 				console.log('res : ', response)
+				question.id = response.name
+				return question
 			})
+			.then(addToLocalStorage)
+			.then(Question.renderList())
+	}
+
+	static renderList() {
+		const questions = getQuestionsFromLocalStorage()
+
+		const html = questions.length 
+		? questions.map(toCard).join('')
+		: '<div> вопросов нет </div>'
+
+		const list = document.getElementById('list')
+
+		list.innerHTML = html
 	}
 }
+
+function addToLocalStorage(question) {
+	const all = getQuestionsFromLocalStorage()
+	all.push(question)
+	localStorage.setItem('questions', JSON.stringify(all))
+}
+
+ 
+function getQuestionsFromLocalStorage() {
+	return JSON.parse(localStorage.getItem('questions') || '[]')
+}
+
+function toCard(question) {
+	return `
+	<div class="mui--text-black-54 mui--text-body2">
+		${new Date(question.date).toLocaleDateString()}
+		${new Date(question.date).toLocaleTimeString()}
+	</div>
+	<div>${question.text}</div>
+	`
+}
+
+
 
